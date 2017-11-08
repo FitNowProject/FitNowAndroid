4709d67057c2e58e2d3e60ad5def2391c287abc5
Dreadds <kevinatc10@gmail.com>
2017-11-08 11:22:27 -0500

    plans activity

--- app/src/main/java/pe/edu/upc/fitnow/activities/LoginActivity.java
+++ app/src/main/java/pe/edu/upc/fitnow/activities/LoginActivity.java
@@ -26,12 +26,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         loginSignInButton.setOnClickListener(this);
     }
 
+    public void loadSlides(View view){
+        new PreferenceManager(this).clearPreferences();
+        startActivity(new Intent(this, PlansActivity.class));
+        finish();
+
+    }
+
     @Override
     public void onClick(View view) {
         new PreferenceManager(this).clearPreferences();
         switch (view.getId()){
             case R.id.loginSignInButton:
                 startActivity(new Intent(this, MainActivity.class));
+                loadSlides(view);
                 break;
             case R.id.signUpNowButton:
                 startActivity(new Intent(this, RegisterActivity.class));
